41b19a94b47ef7c76e70ab2f4df8e235f3ffb7c5
ParanthamaAK <103802729+ParanthamaAK@users.noreply.github.com>
2023-08-23 18:39:58 +0530

    Update DHT11_RPi.py

--- Code/DHT11_RPi.py
+++ Code/DHT11_RPi.py
@@ -1,1 +1,21 @@
+import dht11
+import RPi.GPIO as GPIO
+import time
+
+# Set GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin connected to the data pin of the DHT11 sensor
+DHT11_PIN = 18
+
+# Initialize the DHT11 sensor
+sensor = dht11.DHT11(pin=DHT11_PIN)
+while True:
+    result = sensor.read()
+    print("temperature: ",result.temperature)
+    print("humidity: ",result.humidity)
+    time.sleep(2)
+
+
+
 
